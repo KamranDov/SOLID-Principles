e3c454d0c08241d3c77be61d6f22045e65042c43
Kamran axmedov <82529560+KamranDov@users.noreply.github.com>
2022-12-29 16:07:15 +0400

    Add files via upload

---
+++ HotelCostumers.java
@@ -0,0 +1,11 @@
+package SingleResponsibility;
+
+public class HotelCostumers {
+    public String costumersNames() {
+        return "Musteri adlar siyahisi:";
+    }
+
+    public String addNewCostumers() {
+        return null;
+    }
+}
